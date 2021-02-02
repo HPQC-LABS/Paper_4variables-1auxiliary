729b3935046139cacfdc1f692d00501c4648c1ba
Tina Yu <t98yu@uwaterloo.ca>
2021-02-01 19:28:29 -0500

    Changed 'LHS' and 'RHS' lines to be shorter and more concise.

--- verifications.m
+++ verifications.m
@@ -16,64 +16,40 @@ isequal(LHS,RHS) % Gives 1
 b= dec2bin(2^8-1:-1:0)-'0';
 b1=b(:,1);b2=b(:,2);b3=b(:,3);b4=b(:,4);b5=b(:,5);ba1=b(:,6);ba2=b(:,7);ba3=b(:,8);
 
-blue_LHS= reshape(5*b1.*b2.*b3.*b4 - 3*b1.*b2.*b3 - b1.*b2.*b4 - b1.*b3.*b4 - 2*b2.*b3.*b4,8,[]);
-blue_LHS= blue_LHS(1,:);
+blue_LHS= 5*b1.*b2.*b3.*b4 - 3*b1.*b2.*b3 - b1.*b2.*b4 - b1.*b3.*b4 - 2*b2.*b3.*b4;
+blue_RHS= b1.*b2 + b1.*b3 + 3*b1.*b4 + 2*b2.*b4 + 2*b3.*b4 - ba1.*(5*b1 + 4*b2 + 4*b3 + 6*b4 - 8);
 
-blue_RHS= reshape(b1.*b2 + b1.*b3 + 3*b1.*b4 + 2*b2.*b4 + 2*b3.*b4 - ba1.*(5*b1 + 4*b2 + 4*b3 + 6*b4 - 8),8,[]);
-blue_RHS= min([reshape(blue_RHS(1,:),1,[]);reshape(blue_RHS(5,:),1,[])]);
+green_LHS= 4*b1.*b2.*b3.*b5 - 5.*b1.*b2.*b5 - b1.*b3.*b5 - b2.*b3.*b5;
+green_RHS= (-3)*b1 + 6*b2 - 3*b3 + 5*b5 - 5*b1.*b2 + 3*b1.*b3 - 5*b1.*b5 - b2.*b3 - b3.*b5 - ba2.*(-8*b1 + 6*b2 - 4*b3 + 5*b5 + 3) + 3;
 
-green_LHS= reshape(4*b1.*b2.*b3.*b5 - 5.*b1.*b2.*b5 - b1.*b3.*b5 - b2.*b3.*b5,8,[]);
-green_LHS= green_LHS(1,:);
+red_LHS= 3*b1.*b2.*b4.*b5 - b1.*b4.*b5 - 4*b2.*b4.*b5;
+red_RHS= b1 + 4*b2 + 3*b1.*b2 - b1.*b4 - b1.*b5 - 4*b2.*b4 - 4*b2.*b5 + ba3.*(-4*b1 - 7*b2 + 5*b4 + 5*b5 + 3);
 
-green_RHS= reshape((-3)*b1 + 6*b2 - 3*b3 + 5*b5 - 5*b1.*b2 + 3*b1.*b3 - 5*b1.*b5 - b2.*b3 - b3.*b5 - ba2.*(-8*b1 + 6*b2 - 4*b3 + 5*b5 + 3) + 3,8,[]);
-green_RHS= min([reshape(green_RHS(1,:),1,[]);reshape(green_RHS(3,:),1,[])]);
-
-red_LHS= reshape(3*b1.*b2.*b4.*b5 - b1.*b4.*b5 - 4*b2.*b4.*b5,8,[]);
-red_LHS= red_LHS(1,:);
-
-red_RHS= reshape(b1 + 4*b2 + 3*b1.*b2 - b1.*b4 - b1.*b5 - 4*b2.*b4 - 4*b2.*b5 + ba3.*(-4*b1 - 7*b2 + 5*b4 + 5*b5 + 3),8,[]);
-red_RHS= min([reshape(red_RHS(1,:),1,[]);reshape(red_RHS(2,:),1,[])]);
-
-LHS= blue_LHS + green_LHS + red_LHS;
-RHS= blue_RHS + green_RHS + red_RHS;
+LHS= min(reshape(blue_LHS + green_LHS + red_LHS,8,[]));
+RHS= min(reshape(blue_RHS + green_RHS + red_RHS,8,[]));
 isequal(LHS,RHS) % Gives 1
 
 %% Pg. 6-7, Eqs 32-38
 b= dec2bin(2^10-1:-1:0)-'0';
 b1=b(:,1);b2=b(:,2);b3=b(:,3);b4=b(:,4);b5=b(:,5);ba1=b(:,6);ba2=b(:,7);ba3=b(:,8);ba4=b(:,9);ba5=b(:,10);
 
-blue_LHS= reshape(5*b1.*b2.*b3.*b4 - 3*b1.*b2.*b3 - b1.*b2.*b4 - b1.*b3.*b4 - 2*b2.*b3.*b4,32,[]);
-blue_LHS= blue_LHS(1,:);
+blue_LHS= 5*b1.*b2.*b3.*b4 - 3*b1.*b2.*b3 - b1.*b2.*b4 - b1.*b3.*b4 - 2*b2.*b3.*b4;
+blue_RHS= b1.*b2 + b1.*b3 + 3*b1.*b4 + 2*b2.*b4 + 2*b3.*b4 - ba1.*(5*b1 + 4*b2 + 4*b3 + 6*b4 - 8);
 
-blue_RHS= reshape(b1.*b2 + b1.*b3 + 3*b1.*b4 + 2*b2.*b4 + 2*b3.*b4 - ba1.*(5*b1 + 4*b2 + 4*b3 + 6*b4 - 8),32,[]);
-blue_RHS= min([reshape(blue_RHS(1,:),1,[]);reshape(blue_RHS(17,:),1,[])]);
+red_LHS= 4*b1.*b2.*b3.*b5 - 5*b1.*b2.*b5 - b1.*b3.*b5 - b2.*b3.*b5;
+red_RHS= -3*b1 + 6*b2 - 3*b3 + 5*b5 - 5*b1.*b2 + 3*b1.*b3 - 5*b1.*b5 - b2.*b3 - b3.*b5 - ba2.*(-8*b1 + 6*b2 - 4*b3 + 5*b5 + 3) + 3;
 
-red_LHS= reshape(4*b1.*b2.*b3.*b5 - 5*b1.*b2.*b5 - b1.*b3.*b5 - b2.*b3.*b5,32,[]);
-red_LHS= red_LHS(1,:);
+purple_LHS= 3*b1.*b2.*b4.*b5 - b1.*b4.*b5 - 4*b2.*b4.*b5;
+purple_RHS= b1 + 4*b2 + 3*b1.*b2 - b1.*b4 - b1.*b5 - 4*b2.*b4 - 4*b2.*b5 + ba3.*(-4*b1 - 7*b2 + 5*b4 + 5*b5 + 3);
 
-red_RHS= reshape(-3*b1 + 6*b2 - 3*b3 + 5*b5 - 5*b1.*b2 + 3*b1.*b3 - 5*b1.*b5 - b2.*b3 - b3.*b5 - ba2.*(-8*b1 + 6*b2 - 4*b3 + 5*b5 + 3) + 3,32,[]);
-red_RHS= min([reshape(red_RHS(1,:),1,[]);reshape(red_RHS(9,:),1,[])]);
+green_LHS= 2*b1.*b3.*b4.*b5 - 3*b3.*b4.*b5;
+green_RHS= ba4.*(2*b1 - 3*b3 - 3*b4 - 3*b5 + 6);
 
-purple_LHS= reshape(3*b1.*b2.*b4.*b5 - b1.*b4.*b5 - 4*b2.*b4.*b5,32,[]);
-purple_LHS= purple_LHS(1,:);
+yellow_LHS= b2.*b3.*b4.*b5;
+yellow_RHS= b2.*b3 + b2.*b4 + b2.*b5 + b3.*b4 + b3.*b5 + b4.*b5 + ba5.*(3 - 2*b2 - 2*b3 - 2*b4 - 2*b5);
 
-purple_RHS= reshape(b1 + 4*b2 + 3*b1.*b2 - b1.*b4 - b1.*b5 - 4*b2.*b4 - 4*b2.*b5 + ba3.*(-4*b1 - 7*b2 + 5*b4 + 5*b5 + 3),32,[]);
-purple_RHS= min([reshape(purple_RHS(1,:),1,[]);reshape(purple_RHS(5,:),1,[])]);
-
-green_LHS= reshape(2*b1.*b3.*b4.*b5 - 3*b3.*b4.*b5,32,[]);
-green_LHS= green_LHS(1,:);
-
-green_RHS= reshape(ba4.*(2*b1 - 3*b3 - 3*b4 - 3*b5 + 6),32,[]);
-green_RHS= min([reshape(green_RHS(1,:),1,[]);reshape(green_RHS(3,:),1,[])]);
-
-yellow_LHS= reshape(b2.*b3.*b4.*b5,32,[]);
-yellow_LHS= yellow_LHS(1,:);
-
-yellow_RHS= reshape(b2.*b3 + b2.*b4 + b2.*b5 + b3.*b4 + b3.*b5 + b4.*b5 + ba5.*(3 - 2*b2 - 2*b3 - 2*b4 - 2*b5),32,[]);
-yellow_RHS= min([reshape(yellow_RHS(1,:),1,[]);reshape(yellow_RHS(2,:),1,[])]);
-
-LHS= blue_LHS + red_LHS + purple_LHS + green_LHS + yellow_LHS;
-RHS= blue_RHS + red_RHS + purple_RHS + green_RHS + yellow_RHS;
+LHS= min(reshape(blue_LHS + red_LHS + purple_LHS + green_LHS + yellow_LHS,32,[]));
+RHS= min(reshape(blue_RHS + red_RHS + purple_RHS + green_RHS + yellow_RHS,32,[]));
 isequal(LHS,RHS) % Gives 1
 
 %% Pg. 7, Eqs 43, 46
